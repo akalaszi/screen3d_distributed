0b0f7a4751c79eeaa15458b03103df18b0be67e4
Adrian Kalaszi <kalaszi.adrian@gmail.com>
2014-12-11 21:35:15 +0100

    No need for property molecule handling methods.

--- src/main/java/org/akalaszi/PreprocessMapper.java
+++ src/main/java/org/akalaszi/PreprocessMapper.java
@@ -1,8 +1,6 @@
 package org.akalaszi;
 
 import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -10,7 +8,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import chemaxon.formats.MolExporter;
 import chemaxon.marvin.alignment.MMPAlignment;
-import chemaxon.struc.MPropertyContainer;
 import chemaxon.struc.Molecule;
 
 public class PreprocessMapper extends Mapper<Text, Text, NullWritable, Text> {
@@ -21,21 +18,6 @@ public class PreprocessMapper extends Mapper<Text, Text, NullWritable, Text> {
         Molecule original = MRecordSerializer.fromJSON(mrecord.toString()).toMolecule();
         Molecule m = MMPAlignment.preprocess(original, true);
         context.write(NullWritable.get(), new Text(MolExporter.exportToFormat(m, EXTENSION)));
-    }
-
-    static Map<String, String> propertiesToMap(MPropertyContainer properties) {
-        Map<String, String> props = new LinkedHashMap<String, String>();
-        String[] keys = properties.getKeys();
-        for (String propKey : keys) {
-            props.put(propKey, properties.getObject(propKey).toString());
-        }
-        return props;
-    }
-
-    static void writeProperties(Map<String, String> props, Molecule to) {
-        for (Map.Entry<String, String> entry : props.entrySet()) {
-            to.setProperty(entry.getKey(), entry.getValue());
-        }
     }
 
 }
